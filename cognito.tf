6d3285a8df4ab670f65557f9fa9d610475b75b1d
ed <anax@hotmail.co.uk>
2022-11-19 14:37:49 +0000

    cogito multi-authorizers via map

--- cognito.tf
+++ cognito.tf
@@ -3,15 +3,16 @@
 #
 
 resource "aws_api_gateway_authorizer" "authorizer" {
-  count = var.use_cognito == false ? 0 : 1
+  for_each = var.cognito
 
   rest_api_id = aws_api_gateway_rest_api.this.id
 
   type  = "COGNITO_USER_POOLS"
 
   identity_source = "method.request.header.Authorization"
-  name            = var.gateway_domain
-  provider_arns   = [var.cognito_user_pool_arn]
+  name            = join("-", [var.gateway_domain, each.key])
+  # FIXME: get a list where each.value.type == COGNITO_USER_POOLS
+  provider_arns   = [each.value.authorizer_id]
 
 #  tags = merge(var.project_tags)
 }
--- examples/fargate-cognito/README.md
+++ examples/fargate-cognito/README.md
@@ -1,3 +1,19 @@
 # example cognito authorizer
 
 API Gateway is configured to use a cognito authorizer
+
+Cognito authorizers are set in the `cognito` module variable:
+
+```
+  cognito = {
+    proxy = {
+      authorizer_id = aws_cognito_user_pool.users.arn
+      cognito_domain = join(".", [var.auth_domain, var.project_domain])
+      type = "COGNITO_USER_POOLS"
+    }
+  }
+```
+
++ key is the name of the fargate authorizer group (only `proxy` is valid at the moment).
++ `authorizer_id` is the arn of the cognito user pool
++ `type` must be `COGNITO_USER_POOLS`
